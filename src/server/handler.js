7ecc02410e99e6b1248f56f74c45c51fd5a0fbbd
Dhyoga <dhyogap@gmail.com>
2024-05-19 21:27:14 +0700

    forgot

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,8 +1,8 @@
-const predictClassification = require('../services/inferenceService');
-const crypto = require('crypto');
-const storeData = require('../services/storeData');
-const { Firestore } = require('@google-cloud/firestore');
- 
+const predictClassification = require("../services/inferenceService");
+const crypto = require("crypto");
+const storeData = require("../services/storeData");
+const { Firestore } = require("@google-cloud/firestore");
+
 async function postPredict(request, h) {
   const { image } = request.payload;
   const { model } = request.server.app;
@@ -10,8 +10,9 @@ async function postPredict(request, h) {
   const MAX_SIZE = 1000000; // 1MB dalam byte
   if (image.length > MAX_SIZE) {
     const response = h.response({
-      status: 'fail',
-      message: 'Payload content too length greater than maximum allowed: 1000000'
+      status: "fail",
+      message:
+        "Payload content too length greater than maximum allowed: 1000000",
     });
     response.code(413);
     return response;
@@ -21,60 +22,60 @@ async function postPredict(request, h) {
     const { label, suggestion } = await predictClassification(model, image);
     const id = crypto.randomUUID();
     const createdAt = new Date().toISOString();
-   
+
     const data = {
-      "id": id,
-      "result": label,
-      "suggestion": suggestion,
-      "createdAt": createdAt,
-    }
+      id: id,
+      result: label,
+      suggestion: suggestion,
+      createdAt: createdAt,
+    };
 
     const history = {
-      "result": label,
-      "createdAt": createdAt,
-      "suggestion": suggestion,
-      "id": id,
-    }
+      result: label,
+      createdAt: createdAt,
+      suggestion: suggestion,
+      id: id,
+    };
 
     await storeData(id, history);
-   
+
     const response = h.response({
-      status: 'success',
-      message: 'Model is predicted successfully',
-      data
-    })
+      status: "success",
+      message: "Model is predicted successfully",
+      data,
+    });
     response.code(201);
     return response;
   } catch (error) {
     const response = h.response({
-      status: 'fail',
-      message: 'Terjadi kesalahan dalam melakukan prediksi'
+      status: "fail",
+      message: "Terjadi kesalahan dalam melakukan prediksi",
     });
     response.code(400);
     return response;
-  } 
+  }
 }
 
 const getPredict = async (request, h) => {
   const db = new Firestore();
-  const predictionCollection = db.collection('predictions');
+  const predictionCollection = db.collection("predictions");
   const predictionSnapshot = await predictionCollection.get();
 
   const data = [];
   predictionSnapshot.forEach((doc) => {
-      const history = {
-          id: doc.id,
-          history: doc.data()
-      };
-      data.push(history);
+    const history = {
+      id: doc.id,
+      history: doc.data(),
+    };
+    data.push(history);
   });
 
   const response = h.response({
-      status: 'success',
-      data: data
+    status: "success",
+    data: data,
   });
   response.code(200);
   return response;
-}
+};
 
 module.exports = { postPredict, getPredict };
